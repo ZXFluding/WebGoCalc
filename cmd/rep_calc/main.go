9d0bcc8fdf9b6f8a3dae869241a16ea29432d4a0
GHFluding <spartacocgovaho@gmail.com>
2024-12-23 21:32:53 +0300

    1

---
+++ cmd/rep_calc/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"test/services/config"
+	"test/services/lib/sl"
+	"test/services/repositories/postgres"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	//init config: env
+	cfg := config.MustLoad()
+
+	//init logger: slog
+	log := setupLogger(cfg.Env)
+
+	//Logger is up
+	log.Info("starting rep_cal", slog.String("env", cfg.Env))
+	log.Debug("debug masseges are enabled")
+	//TODO: init Repositories: postgrers
+	repository, err := postgres.New(cfg.RepositoryPath)
+	if err != nil {
+		log.Error("failed to init db: ", sl.Err(err))
+		os.Exit(1)
+	}
+	_ = repository
+	//TODO: init router: gin-gonic
+
+	//TODO: init controllers
+
+	//TODO: init Services
+
+}
+
+// setup Logger configuraton
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelInfo},
+			),
+		)
+	}
+	return log
+}
---
+++ services/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env            string `yaml:"env" env-default:"local"` //env-required:"true"
+	RepositoryPath string `yaml:"repository_path" env-required:"true"`
+	HTTPServer     `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8082"`
+	TimeOut     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeOut time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatalln("config_path is not set")
+	}
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config_path is not exist: %s", configPath)
+	}
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalln("cannot read config")
+	}
+	return &cfg
+}
